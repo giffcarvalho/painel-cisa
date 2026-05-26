69616fc9452312441f1f91f55a52bd1b113f7d70
andre.ide <andre.ide@cidades.gov.br>
2026-05-26 20:17:20 -0300

    Ajuste nos filtros das rotas do mapa

--- backend/app/api/mapa.py
+++ backend/app/api/mapa.py
@@ -8,17 +8,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError
 from app.core.database import get_db
 import jenkspy
+import time
 from app.schemas.filtrosMapa import BuscaFiltroResponse, OpcoesFiltros
+
 
  
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
-CAMPOS_BUSCA_FILTROS = {
-    "municipio": "cod_municipio::text",
-    "uf": "cod_uf::text",
-    "nr_instrumento": "nr_instrumento::text",
-}
 
 
 async def _execute_query(db: AsyncSession, sql: str, params: dict | None = None) -> CursorResult: #--Executa a query com tratamento de erro para evitar que exceções brutas do banco vazem.
@@ -36,42 +33,53 @@ async def _execute_query(db: AsyncSession, sql: str, params: dict | None = None)
 class FiltrosMapa:  #-- Dependência do FastAPI para agrupar todos os Query Parameters. Evita repetição nas assinaturas das funções de rota.
     def __init__(
             self,
-            uf: list[str] | None = Query(None),
-            municipio: list[str] | None = Query(None),
+            cod_uf: list[int] | None = Query(None), #-- list[] é porque pode ser mais de um, | None diz que é opcional, e Query(None) diz que não tem valor padrão
+            cod_municipio: list[int] | None = Query(None),
             nr_instrumento: list[str] | None = Query(None),
             tipo_instrumento: list[str] | None = Query(None),
             acao_padronizada: list[str] | None = Query(None) 
     ):
-        self.uf = uf
-        self.municipio = municipio
+        self.cod_uf = cod_uf
+        self.cod_municipio = cod_municipio
         self.nr_instrumento = nr_instrumento
         self.tipo_instrumento = tipo_instrumento
         self.acao_padronizada = acao_padronizada
         
 
-def _build_where(filtros: FiltrosMapa) -> tuple[str, dict]:
-    clauses: list[str] = []
-    params: dict = {}
+def _build_where(filtros: FiltrosMapa) -> tuple[str, dict]:  #--filtros: FiltrosMapa é recebido do frontend via depends. str é a string da cláusula where, dict é o dicionário com os valores dos params
+    clauses: list[str] = []                                  #--é list porque é a junção de várias strings que vão ser concatenadas p/ formar a clásula where final
+    params: dict = {}                                        #--é o dicionário com os valores dos params que foram recebidos do frontend
 
-    list_filters = [
-        ("cod_uf", filtros.uf, "cod_uf", "text"),
-        ("cod_municipio", filtros.municipio, "cod_municipio", "text"),
-        ("nr_instrumento", filtros.nr_instrumento, "nr_instrumento", "text"),
-        ("tipo_instrumento", filtros.tipo_instrumento, "tipo_instrumento", None),
+    
+    
+    array_filters = [                                        #--algumas colunas dessa rota são arrays
+        ("cod_uf", filtros.cod_uf),
+        ("cod_municipio", filtros.cod_municipio),
+    ]
+
+    normal_filters = [
+        ("nr_instrumento", filtros.nr_instrumento, "nr_instrumento", int),              #--na sequência (coluna do banco, valor recebido, nome do params, cast)
+        ("tipo_instrumento", filtros.tipo_instrumento, "tipo_instrumento", None),       #--quando é string no banco, não precisa de cast, pois frontend já entrega string
         ("acao_padronizada", filtros.acao_padronizada, "acao_padronizada", None)
     ]
 
-    for col, values, param_key, cast_type in list_filters:
+
+    for col, values in array_filters:                                                   #--construção das clauses para colunas que são array no banco
         if values:
-            placeholder = ", ".join(f":{param_key}_{i}" for i in range(len(values)))
-            sql_col = f"{col}::{cast_type}" if cast_type else col
-            clauses.append(f"{sql_col} IN ({placeholder})")
+            clauses.append(f"{col} && :{col}")
+            params[col] = values
+
+
+    for col, values, param_key, cast_python in normal_filters:                          #--construção das clauses para colunas que não são array no banco
+        if values:
+            placeholder = ", ".join(f":{param_key}_{i}" for i in range(len(values)))    #--gera :cod_uf_0, :cod_uf_1, :cod_uf_2, etc isso vai compor a cláusula where depois
+            clauses.append(f"{col} IN ({placeholder})")                                 #--gera um pedaço da clásula where: cod_uf in (:cod_uf_0, :cod_uf_1, :cod_uf_2)
 
             for i, v in enumerate(values):
-                params[f"{param_key}_{i}"] = str(v).strip()
+                params[f"{param_key}_{i}"] = (cast_python(v) if cast_python else v)     #--cria o dicionário params, por exemplo: params["uf_0"] = "SP", params["uf_1"] = "RJ"
 
-    where = ("WHERE " + " AND ".join(clauses)) if clauses else ""
-    return where, params
+    where = (" AND ".join(clauses)) if clauses else ""                       #--cria a classe where final concatenando as clauses, separando por AND
+    return where, params                                                                #--retorno da função -build_where, a clausula WHERE final o dicionário com os valores dos params
 
 
 # ufs
@@ -381,6 +389,7 @@ async def get_municipios_2022(z: int, x: int, y: int, db: AsyncSession = Depends
 
 # classificação das classes cloropléticas do mapa usando quebra Jenks (lib jenkspy)
 cache_jenks = {}
+CACHE_TTL = 3600
 colunas_permitidas = [
     "populacao_total_censo_2022",
     "categoria_metropolitana",
@@ -399,14 +408,21 @@ colunas_permitidas = [
     "deficit_residuo_urbana_ibge",
     "deficit_banheiro_urbana_ibge",
 ]
-
+    
 @router.get("/classificacao/{variavel}")
 async def get_classificacao(variavel: str, classes: int = 5, db: AsyncSession = Depends(get_db)):
 
-    cache_key = f"{variavel}_{classes}"
+    
+    cache_key = (variavel, classes)
+    cached = cache_jenks.get(cache_key)
 
-    if cache_key in cache_jenks:
-        return cache_jenks[cache_key]
+    if cached:
+        data, expires_at = cached
+
+        if time.time() < expires_at:
+            return data
+
+        del cache_jenks[cache_key]
 
 
     if classes < 2 or classes > 9:
@@ -438,7 +454,11 @@ async def get_classificacao(variavel: str, classes: int = 5, db: AsyncSession = 
         "breaks": breaks
     }
 
-    cache_jenks[cache_key] = resultado
+    cache_jenks[cache_key] = (
+        resultado,
+        time.time() + CACHE_TTL
+    )
+    
     return resultado
 
 
@@ -449,26 +469,27 @@ async def get_empreendimentos_dsr(z: int, x: int, y: int, filtros: FiltrosMapa =
 
     
     tile_params = {"z": z, "x": x, "y": y}
-    filter_where, filter_params = _build_where(filtros)
+    filter_clauses, filter_params = _build_where(filtros)
     spatial_where = """geom && ST_Transform(ST_TileEnvelope(:z, :x, :y), 4326)"""
 
-    if filter_where:
-        where = f"""WHERE {spatial_where} AND {filter_where.removeprefix("WHERE ")}"""
-    else:
-        where = f"WHERE {spatial_where}"
+    where_clauses = [spatial_where]
+
+    if filter_clauses:
+        where_clauses.append(filter_clauses)
+
+    where = "WHERE " + " AND ".join(where_clauses)
 
     params = {**tile_params, **filter_params}
-
 
     sql = f"""
         SELECT ST_AsMVT(tile, 'pontos', 4096, 'geom') AS mvt
         FROM (
             SELECT
+                cod_uf,
+                cod_municipio,
                 nr_instrumento,
                 tipo_instrumento,
                 acao_padronizada,
-                cod_uf,
-                cod_municipio,
                 ST_AsMVTGeom(ST_Transform(geom, 3857), ST_TileEnvelope(:z, :x, :y), 4096, 256, true) AS geom
             FROM temporario.vw_coordenadas
             {where}
