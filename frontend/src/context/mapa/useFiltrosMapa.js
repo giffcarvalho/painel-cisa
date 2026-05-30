823c4c94e94cd583dd837a6c3a85df3d98089092
andre.ide <andre.ide@cidades.gov.br>
2026-05-30 07:48:14 -0300

    Exclusão em função de unificação do context dos filtros do mapa em arquivo único
    Posteriormente volto a fazer a divisão de responsabilidades dividindo em arquivos separados

--- frontend/src/context/mapa/useFiltrosMapa.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { FiltrosContext } from './filtrosContextValue'
-
-export function useFiltrosMapa() {
-  const ctx = useContext(FiltrosContext)
-  if (!ctx) throw new Error('useFiltros deve ser usado dentro de FiltrosProvider')
-  return ctx
-}
