d91191b3dc716571f833109aff98cdbeaeb4219b
andre.ide <andre.ide@cidades.gov.br>
2026-05-30 07:42:49 -0300

    Inclusão das urls que buscam as geometrias

--- frontend/src/api/mapa.js
+++ frontend/src/api/mapa.js
@@ -18,9 +18,75 @@ function toParams(filtros) {
   return params
 }
 
-export const mapaApi = {
-  getFiltros:       ()        => api.get('/mapa/filtros'),
-  getLocalidade:    (filtros) => api.get('/mapa/graficos/localidade', { params: toParams(filtros) }),
-  getFaseSituacao:  (filtros) => api.get('/mapa/graficos/fase-situacao', { params: toParams(filtros) }),
-  buscarFiltro: (campo, q, limit = 50) => api.get('/mapa/filtros/busca', { params: { campo, q, limit }, }),
+
+
+export async function listarUfs(filtros = {}) {
+
+  const res = await api.get("/mapa/filtros/ufs", {params: toParams(filtros)});
+  
+  return res.data.data;
+}
+
+
+export async function listarMunicipios(q="", cod_uf, limit = 20) {
+  
+  const res = await api.get("/mapa/filtros/municipios", {params: { q, cod_uf, limit }});
+
+  return res.data.data;
+}
+
+
+const API_URL = "http://localhost:8000/api/v1"
+
+export function urlBboxUfs(filtros={}) {
+  const params = toParams(filtros);
+  return `${API_URL}/mapa/bbox_ufs?${params}`
+}
+
+
+export function urlBboxMunicipios(filtros={}) {
+  const params = toParams(filtros);
+  return `${API_URL}/mapa/bbox_municipios?${params}`
+}
+
+
+export function urlUfs(filtros={}) {
+  const params = toParams(filtros);
+  return `${API_URL}/mapa/ufs/{z}/{x}/{y}.pbf?${params}`
+}
+
+
+export function urlMunicipios2025(filtros={}) {
+  const params = toParams(filtros);
+  return `${API_URL}/mapa/municipios_2025/{z}/{x}/{y}.pbf?${params}`
+}
+
+
+export function urlDistritos2022(filtros={}) {
+  const params = toParams(filtros);
+  return `${API_URL}/mapa/distritos_2022/{z}/{x}/{y}.pbf?${params}`
+}
+
+
+export function urlSetoresCensitarios2022(filtros={}) {
+  const params = toParams(filtros);
+  return `${API_URL}/mapa/setores_censitarios_2022/{z}/{x}/{y}.pbf?${params}`
+}
+
+
+export function urlLocalidades2022(filtros={}) {
+  const params = toParams(filtros);
+  return `${API_URL}/mapa/localidades_2022/{z}/{x}/{y}.pbf?${params}`
+}
+
+
+export function urlEnderecos2022(filtros={}) {
+  const params = toParams(filtros);
+  return `${API_URL}/mapa/enderecos_2022/{z}/{x}/{y}.pbf?${params}`
+}
+
+
+export function urlMunicipios2022(filtros={}) {
+  const params = toParams(filtros);
+  return `${API_URL}/mapa/municipios_2022/{z}/{x}/{y}.pbf?${params}`
 }
