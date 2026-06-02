e85e9588c2d5936b5421559e0e60994ef0075557
andre.ide <andre.ide@cidades.gov.br>
2026-06-02 14:45:03 -0300

    Correção de conflitos

--- frontend/src/pages/carteira-dsr/CarteiraDsr.jsx
+++ frontend/src/pages/carteira-dsr/CarteiraDsr.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useFiltros } from '@/context/useFiltros'
+import { useFiltros } from "@/context/carteira-Dsr/useFiltros"
 import { Database, Table, SlidersHorizontal } from 'lucide-react'
 import KpisSection from '@/components/carteira-dsr/KpisSection'
 import GraficosSection from '@/components/carteira-dsr/GraficosSection'
