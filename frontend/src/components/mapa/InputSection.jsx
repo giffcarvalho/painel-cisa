c0b16902717b0dd8e3b209111498d59a32f1fe29
andre.ide <andre.ide@cidades.gov.br>
2026-06-14 20:03:42 -0300

    Ajuste no botão que limpa a coordenada criada

--- frontend/src/components/mapa/InputSection.jsx
+++ frontend/src/components/mapa/InputSection.jsx
@@ -1,28 +1,29 @@
 import estilos from "./InputSection.module.css";
 import Input from "./Input";
-import { Navigation } from "lucide-react";
+import { Navigation, X } from "lucide-react";
 
 
-export default function InputSection({ coord = { lat: "", long: "" }, setCoord = () => {}, irParaCoordenada }) {
+export default function InputSection({ coord = { lat: "", long: "" }, setCoord = () => {}, irParaCoordenada, limparCoordenada }) {
 
     return (
-        <div className={estilos.inputSection}>
+        <form className={estilos.inputSection} onSubmit={(e) => {e.preventDefault(); irParaCoordenada();}}>
             <Input
-                place_holder = "Latitude"
+                place_holder = "Lat"
                 value = {coord.lat}
                 onChange={val => setCoord(prev => ({ ...prev, lat: val }))}
             />
 
             <Input
-                place_holder = "Longitude"
+                place_holder = "Long"
                 value = {coord.long}
                 onChange={val => setCoord(prev => ({ ...prev, long: val }))}
             />
 
-            <button className={estilos.botaoCoordenadas} onClick={() => irParaCoordenada()}> <Navigation className={estilos.IrIcon}/> </button>
+            <button type="submit" className={estilos.botaoCoordenadas}> <Navigation className={estilos.IrIcon}/> </button>
+            <button type="button" className={estilos.botaoCoordenadas} onClick={() => limparCoordenada()}> <X className={estilos.XIcon}/> </button>
 
             
-        </div>
+        </form>
 
 
     )
